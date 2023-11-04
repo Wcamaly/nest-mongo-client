1df8c8154e93df7deff0cf5187f52c6c17d45dc4
wcamaly <walter.camaly@coderhouse.com>
2023-11-04 12:47:31 -0300

    fix linting and ts

--- src/mongo.providers.ts
+++ src/mongo.providers.ts
@@ -1,10 +1,13 @@
-import { Db } from 'mongodb'
-import { getCollectionToken, getDbToken } from './mongo.util'
+import { Db } from 'mongodb';
+import { getCollectionToken, getDbToken } from './mongo.util';
 
-export function createMongoProviders(connectionName?: string, collections: string[] = []) {
-    return collections.map(collectionName => ({
-        provide: getCollectionToken(collectionName),
-        useFactory: (db: Db) => db.collection(collectionName),
-        inject: [getDbToken(connectionName)]
-    }))
+export function createMongoProviders(
+  connectionName?: string,
+  collections: string[] = [],
+) {
+  return collections.map((collectionName) => ({
+    provide: getCollectionToken(collectionName),
+    useFactory: (db: Db) => db.collection(collectionName),
+    inject: [getDbToken(connectionName)],
+  }));
 }
--- src/mongo.util.ts
+++ src/mongo.util.ts
@@ -1,19 +1,23 @@
-import { DEFAULT_MONGO_CONNECTION_NAME } from './mongo.constants'
+import { DEFAULT_MONGO_CONNECTION_NAME } from './mongo.constants';
 
 /**
  * Get a token for the MongoClient object for the given connection name
  * @param connectionName The unique name for the connection
  */
-export function getClientToken(connectionName: string = DEFAULT_MONGO_CONNECTION_NAME) {
-    return `___${toSnackCase(connectionName).toUpperCase()}_CLIENT___`
+export function getClientToken(
+  connectionName: string = DEFAULT_MONGO_CONNECTION_NAME,
+) {
+  return `___${toSnackCase(connectionName).toUpperCase()}_CLIENT___`;
 }
 
 /**
  * Get a token for the Mongo Db object for the given connection name
  * @param connectionName The unique name for the connection
  */
-export function getDbToken(connectionName: string = DEFAULT_MONGO_CONNECTION_NAME) {
-    return `___${toSnackCase(connectionName).toUpperCase()}_DATABASE___`
+export function getDbToken(
+  connectionName: string = DEFAULT_MONGO_CONNECTION_NAME,
+) {
+  return `___${toSnackCase(connectionName).toUpperCase()}_DATABASE___`;
 }
 
 /**
@@ -21,9 +25,9 @@ export function getDbToken(connectionName: string = DEFAULT_MONGO_CONNECTION_NAM
  * @param collectionName The unique name for the collection
  */
 export function getCollectionToken(collectionName: string) {
-    return `___${toSnackCase(collectionName).toUpperCase()}_COLLECTION___`
+  return `___${toSnackCase(collectionName).toUpperCase()}_COLLECTION___`;
 }
 
-function toSnackCase (str: string) : string {
-  return str.replace(/([a-z])([A-Z])/g, '$1_$2')
+function toSnackCase(str: string): string {
+  return str.replace(/([a-z])([A-Z])/g, '$1_$2');
 }
